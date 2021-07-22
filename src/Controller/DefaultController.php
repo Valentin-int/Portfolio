a739575be8d3bc235bb3fcef110e408f0c1b15c8
Valentin <valentin.lay98@gmail.com>
2021-07-22 14:41:59 +0200

    style contact et ajout background-image sur les card admin

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +14,20 @@ Class DefaultController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('index/index.html.twig');
+        $message = new Message();
+        $formContact = $this->createForm(MessageType::class, $message);
+        $formContact->handleRequest($request);
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            return $this->redirect($request->getUri());
+        }
+        return $this->render('index/index.html.twig',[
+            'formContact' => $formContact->createView(),
+        ]);
     }
 }
