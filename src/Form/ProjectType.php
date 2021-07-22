b2df1941d87f7adfb8c39507c381d3defc424544
Valentin <valentin.lay98@gmail.com>
2021-07-22 16:29:47 +0200

    style index

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +15,19 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('url_project')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du projet :',
+            ])
+            ->add('url_project', UrlType::class, [
+                'label' => 'Url du projet :',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description :',
+            ])
+            // ->add('images', ImageType::class, [
+            //     'label' => false,
+            //     'data_class' => null,
+            // ])
         ;
     }
 
